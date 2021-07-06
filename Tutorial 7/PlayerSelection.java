62ddb3c5b3f9d2d8ac39400cef6cf174f0b6f1b1
sara7869 <86092128+sara7869@users.noreply.github.com>
2021-07-06 15:02:41 +0530

    Initial Commit

---
+++ Tutorial 7/PlayerSelection.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class PlayerSelection {
+
+    public static void main(String[] args) {
+            ArrayList<Player> PlayerArrayList = new ArrayList<Player>();
+
+            Player player1= new Player();
+            Player player2= new Player();
+            Player player3= new Player();
+            Player player4= new Player();
+            Player player5= new Player();
+
+            PlayerArrayList.add(player1);
+            PlayerArrayList.add(player2);
+            PlayerArrayList.add(player3);
+            PlayerArrayList.add(player4);
+            PlayerArrayList.add(player5);
+
+        }
+}
