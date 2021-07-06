98d4cb0a12e0fa2a780e115b5e7450987f1b7734
pavi-ravi26 <87009429+pavi-ravi26@users.noreply.github.com>
2021-07-06 15:25:44 +0530

    Update menu.java

--- menu.java
+++ menu.java
@@ -1,1 +1,16 @@
+import java.util.*;
+public class Main {
 
+    public static void main(String[] args) {
+	   Scanner input=new Scanner(System.in);
+	   System.out.println("A- add a player");
+	   System.out.println("V- view all player");
+
+	   public void addplayer(){
+	   	
+		}
+       public void viewplayer(){
+	   	
+		} 
+    }
+}
