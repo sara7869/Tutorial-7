fe1ddba15d7dd15fe61f9fdbfdb62f429067d328
sara7869 <86092128+sara7869@users.noreply.github.com>
2021-07-06 15:17:56 +0530

    Final Changes to ArrayList part

--- Tutorial 7/PlayerSelection.java
+++ Tutorial 7/PlayerSelection.java
@@ -5,11 +5,11 @@ public class PlayerSelection {
     public static void main(String[] args) {
             ArrayList<Player> PlayerArrayList = new ArrayList<Player>();
 
-            Player player1= new Player();
-            Player player2= new Player();
-            Player player3= new Player();
-            Player player4= new Player();
-            Player player5= new Player();
+            Player player1= new Player("Dawid Malan",33 , "batsman" , 265 , 103);
+            Player player2= new Player("Aaron finch" , 34 , "batsman" , 350 , 172);
+            Player player3= new Player("Babar Azam" , 26 , "batsman" , 354 , 122);
+            Player player4= new Player("Dewon Conway" , 29 , "batsman" , 335 , 99);
+            Player player5= new Player("Virat Kohli", 32 , "batsman" , 390 , 94);
 
             PlayerArrayList.add(player1);
             PlayerArrayList.add(player2);
